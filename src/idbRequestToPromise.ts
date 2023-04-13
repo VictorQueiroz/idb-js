29f37e615b727b438d80c33b71abde85fe748d59
Victor Queiroz <victorcqueirozg@gmail.com>
2023-04-13 06:11:32 -0300

    only add event execute listeners if `value` method is called

--- src/idbRequestToPromise.ts
+++ src/idbRequestToPromise.ts
@@ -1,19 +1,31 @@
-export default function idbRequestToPromise<T>(getReq: () => IDBRequest<T>) {
-  return new Promise<T | null>((resolve) => {
-    let req: IDBRequest<T>;
-    try {
-      req = getReq();
-    } catch (reason) {
-      console.error("Failed to create request with error: %o", reason);
-      resolve(null);
-      return;
-    }
-    req.onerror = () => {
-      console.error("Request failed with the following error: %o", req.error);
-      resolve(null);
+export interface IRequest<T> {
+  value(): Promise<T | null>;
+}
+
+export default function idbRequestToPromise<T>(
+  getReq: () => IDBRequest<T>
+): IRequest<T> {
+  let req: IDBRequest<T>;
+  try {
+    req = getReq();
+  } catch (reason) {
+    console.error("Failed to create request with error: %o", reason);
+    return {
+      value: async () => null,
     };
-    req.onsuccess = () => {
-      resolve(req.result ?? null);
-    };
-  });
+  }
+  return {
+    value: () =>
+      new Promise<T | null>((resolve) => {
+        req.onerror = () => {
+          console.error(
+            "Request failed with the following error: %o",
+            req.error
+          );
+        };
+        req.onsuccess = () => {
+          resolve(req.result ?? null);
+        };
+      }),
+  };
 }
